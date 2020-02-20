dbc4a2d9b6fec66d1262a6302442923b0f01283e
Thibaut MINARD <minard.thibaut@free.fr>
2020-02-20 17:59:42 +0100

    Update MatchView.php

--- view/FrontEnd/MatchView.php
+++ view/FrontEnd/MatchView.php
@@ -10,15 +10,18 @@
 
     <div id="match_teams" class="d-flex flex-row justify-content-center">
         <div id="match_team_home" class="d-flex flex-column align-items-center justify-content-end col-6">
-            <div id ="match_team_home-logo" class="col-6">
+            <div id ="match_team_home-logo" class="col-5">
                 <img src="public/img/logo.png" class="img-responsive fit-image">
             </div>
             <div id ="match_team_home-name">
                 E113 Football Club
             </div>
         </div>
+        <div id="match_score" class="col-2">
+            3-0
+        </div>    
         <div id="match_team_away" class="d-flex flex-column align-items-center col-6">
-            <div id ="match_team_away-logo" class="col-6">
+            <div id ="match_team_away-logo" class="col-5">
                 <img src="public/img/logo_sl.svg" class="img-responsive fit-image">
             </div>
             <div id ="match_team_away-name">
@@ -27,37 +30,44 @@
         </div>
     </div>
     
-    <div id="match_data" class="m-4 p-4 col-8 border border-primary rounded-lg myborder">
-        <p id="match_address">
-            <span class="player_data-title font-weight-bold">Terrain :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-            <span class="player_data-txt">28 rue des cèpes 53210 Argentré</span>
-        </p>
-        <p id="match_field">
-            <span class="player_data-title font-weight-bold">Pelouse :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-            <span class="player_data-txt">Naturelle</span>
-        </p>
-        <p id="match_meteo">
-            <span class="player_data-title font-weight-bold">Prévision météo :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-            <span class="player_data-txt">Soleil</span>
-        </p>
-    </div>  
+    <div id="pre_match">
+        <div id="match_data" class="m-4 p-4 col-8 border border-primary rounded-lg myborder">
+            <p id="match_address">
+                <span class="player_data-title font-weight-bold">Terrain :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                <span class="player_data-txt">28 rue des cèpes 53210 Argentré</span>
+            </p>
+            <p id="match_field">
+                <span class="player_data-title font-weight-bold">Pelouse :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                <span class="player_data-txt">Naturelle</span>
+            </p>
+            <p id="match_meteo">
+                <span class="player_data-title font-weight-bold">Prévision météo :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                <span class="player_data-txt">Soleil</span>
+            </p>
+        </div>  
 
-    <div id="match_player" class="m-4 p-4 col-8 border border-primary rounded-lg myborder">
-        <div id="match_player-title">Joueurs convoqués</div>
-        <ul>
-            <li>Nom Prénom</li>
-            <li>Nom Prénom</li>
-            <li>Nom Prénom</li>
-            <li>Nom Prénom</li>
-            <li>Nom Prénom</li>
-            <li>Nom Prénom</li>
-            <li>Nom Prénom</li>
-            <li>Nom Prénom</li>
-            <li>Nom Prénom</li>
-            <li>Nom Prénom</li>
-            <li>Nom Prénom</li>
-        </ul>
+        <div id="match_player" class="m-4 p-4 col-8 border border-primary rounded-lg myborder">
+            <div id="match_player-title">Joueurs convoqués</div>
+            <ul>
+                <li>Nom Prénom</li>
+                <li>Nom Prénom</li>
+                <li>Nom Prénom</li>
+                <li>Nom Prénom</li>
+                <li>Nom Prénom</li>
+                <li>Nom Prénom</li>
+                <li>Nom Prénom</li>
+                <li>Nom Prénom</li>
+                <li>Nom Prénom</li>
+                <li>Nom Prénom</li>
+                <li>Nom Prénom</li>
+            </ul>
+        </div>
     </div>
+
+    <div id="post_match">
+
+
+
 </div>
 
 
